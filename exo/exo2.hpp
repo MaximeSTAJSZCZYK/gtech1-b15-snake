5f9514fd07d483cb343108f94cb9e8f4047db22d
MAXIME <mstajszczyk@gaming.tech>
2022-01-25 17:34:06 +0100

    Window

---
+++ exo/exo2.hpp
@@ -0,0 +1,19 @@
+#ifndef exo2_HPP
+#define exo2_HPP
+
+class exo2
+{
+private:
+    /* data */
+public:
+    exo2(/* args */);
+    ~exo2();
+};
+
+exo2::exo2(/* args */)
+{
+}
+
+exo2::~exo2()
+{
+}
