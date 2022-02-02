a769dda797ecac2438c341f32e41c98b0cea4348
MAXIME <mstajszczyk@gaming.tech>
2022-02-02 17:59:30 +0100

    Menu complet

--- maxime/exo/snake1.cpp
+++ maxime/exo/snake1.cpp
@@ -5,17 +5,71 @@
 #include <iostream>
 #include <list>
 
+#define HeightTextTitle SCREEN_HEIGHT/100*2
+#define HeightText SCREEN_HEIGHT/100*1
+
 int main(int argc, char *argv[])
 {
     SDL_Window* fenetre;
     SDL_Renderer* renderer;
+
+    int row;
+    int column;
+    int SCREEN_HEIGHT = 600;
+    int SCREEN_WIDTH = 600;
+
     //int menu[];
-    int start[][25] = {
-                    {0,0,1,1,1,0,1,1,1,0,0,1,1,0,0,1,1,1,0,0,1,1,1,0},
-                    {0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,1,0,0,1,0,0,1,0,0},
-                    {0,0,1,1,0,0,0,1,0,0,1,1,1,1,0,1,1,1,0,0,0,1,0,0},
-                    {0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,1,0,1,0,0,0,1,0,0},
-                    {0,1,1,1,0,0,0,1,0,0,1,0,0,1,0,1,0,0,1,0,0,1,0,0}};
+
+    int start[5][50] = {
+                    {0,1,1,1,0,1,1,1,0,0,1,0,0,1,1,1,0,0,1,1,1},
+                    {1,0,0,0,0,0,1,0,0,1,0,1,0,1,0,0,1,0,0,1,0},
+                    {0,1,1,0,0,0,1,0,0,1,1,1,0,1,1,1,0,0,0,1,0},
+                    {0,0,0,1,0,0,1,0,0,1,0,1,0,1,0,1,0,0,0,1,0},
+                    {1,1,1,0,0,0,1,0,0,1,0,1,0,1,0,0,1,0,0,1,0}};
+                  
+
+    int snake[5][50] = {
+                    {0,1,1,1,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,1,1,1,0,1},
+                    {1,0,0,0,0,1,1,0,1,0,1,0,1,0,1,0,1,0,0,1,0,0,0,1},
+                    {0,1,1,0,0,1,0,1,1,0,1,1,1,0,1,1,0,0,0,1,1,1,0,1},
+                    {0,0,0,1,0,1,0,0,1,0,1,0,1,0,1,0,1,0,0,1,0,0,0,0},
+                    {1,1,1,0,0,1,0,0,1,0,1,0,1,0,1,0,0,1,0,1,1,1,0,1}}; 
+
+    int option[5][50] = {
+                    {0,1,1,0,0,0,1,1,0,0,1,1,1,0,1,0,0,1,1,0,0,1,0,0,1},
+                    {1,0,0,1,0,1,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,1,1,0,1},
+                    {1,0,0,1,0,1,1,1,0,0,0,1,0,0,1,0,1,0,0,1,0,1,0,1,1},
+                    {1,0,0,1,0,1,0,0,0,0,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1},
+                    {0,1,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,1,0,0,1,0,0,1}}; 
+
+    int leave[5][50] = {
+                    {1,0,0,0,1,1,1,0,0,1,0,0,1,0,1,0,1,1,1,0,0},
+                    {1,0,0,0,1,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,0},
+                    {1,0,0,0,1,1,1,0,1,1,1,0,1,0,1,0,1,1,1,0,0},
+                    {1,0,0,0,1,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,0},
+                    {1,1,1,0,1,1,1,0,1,0,1,0,0,1,0,0,1,1,1,0,0}}; 
+
+    int gameover[5][50] = {
+                    {0,1,1,1,0,0,1,0,0,1,0,0,0,1,0,1,1,1,0,0,1,1,1,0,1,0,1,0,1,1,1,0,1,1,1,0,0,0},
+                    {1,0,0,0,0,1,0,1,0,1,1,0,1,1,0,1,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,1,0,0,1,0,0},
+                    {1,0,1,1,0,1,1,1,0,1,0,1,0,1,0,1,1,1,0,0,1,0,1,0,1,0,1,0,1,1,1,0,1,1,1,0,0,0},
+                    {1,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,0,0},
+                    {0,1,1,0,0,1,0,1,0,1,0,0,0,1,0,1,1,1,0,0,1,1,1,0,0,1,0,0,1,1,1,0,1,0,0,1,0,1}}; 
+
+    int numbers[5][60] = {
+                    {1,1,1,0,0,1,0,0,1,1,0,0,1,1,0,0,1,0,1,0,1,1,1,0,1,1,1,0,1,1,0,0,1,1,1,0,1,1,1,0,0},
+                    {1,0,1,0,1,1,0,1,0,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,0,0,0,1,0,0,1,0,1,0,1,0,1,0,0},
+                    {1,0,1,0,0,1,0,0,0,1,0,0,0,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,0},
+                    {1,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0,1,0,0,1,0,0,1,0,1,0,0,0,1,0,0},
+                    {1,1,1,0,0,1,0,1,1,1,1,0,1,1,0,0,0,0,1,0,1,1,1,0,1,1,1,0,0,1,0,0,1,1,1,0,1,1,1,0,0}}; 
+
+    int arrow[5][10] = {
+                    {0,0,0,1,0,0},
+                    {0,0,0,0,1,0},
+                    {1,1,1,1,1,1},
+                    {0,0,0,0,1,0},
+                    {0,0,0,1,0,0}}; 
+
 
 
 if(SDL_VideoInit(NULL) < 0)
@@ -25,7 +79,7 @@ if(SDL_VideoInit(NULL) < 0)
 }
 
 
-fenetre = SDL_CreateWindow("Carré" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , 1000 , 1000 , SDL_WINDOW_RESIZABLE);
+fenetre = SDL_CreateWindow("Carré" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , SCREEN_HEIGHT , SCREEN_WIDTH , SDL_WINDOW_RESIZABLE);
 if(fenetre == NULL)
 {
     printf("Erreur lors de la creation d'une fenetre : %s",SDL_GetError());
@@ -40,25 +94,121 @@ if(renderer == NULL)
     return EXIT_FAILURE;
 }
 
-    SDL_SetRenderDrawColor(renderer,255,255,255,255);
-    
-for (int i = 0; i < 5; i++)
+
+row = sizeof(snake)/sizeof(snake[0]);
+column = sizeof(snake[0])/row;
+for (int i = 0; i < row; i++)
 {
-   for (int k = 0; k < sizeof(start); k++) {
-    if(start[i][k] == 1){
-        SDL_Rect rect = {150+(k*10),300+(i*10),10,10};
+   for (int k = 0; k < column; k++) {
+    if(snake[i][k] == 1){
+        SDL_SetRenderDrawColor(renderer,50,200,50,0);
+        SDL_Rect rect = {SCREEN_WIDTH/100*30+(k*HeightTextTitle),SCREEN_HEIGHT/100*10+(i*HeightTextTitle),HeightTextTitle,HeightTextTitle};
         SDL_RenderDrawRect(renderer, &rect);
         SDL_RenderFillRect(renderer, &rect);
     }
 }
 }
 
+row = sizeof(start)/sizeof(start[0]);
+column = sizeof(start[0])/row;
+for (int i = 0; i < row; i++)
+{
+   for (int k = 0; k < column; k++) {
+    if(start[i][k] == 1){
+        SDL_SetRenderDrawColor(renderer,255,255,255,255);
+        SDL_Rect rect = {SCREEN_WIDTH/100*40+(k*HeightText),SCREEN_HEIGHT/100*30+(i*HeightText),HeightText,HeightText};
+        SDL_RenderDrawRect(renderer, &rect);
+        SDL_RenderFillRect(renderer, &rect);
+    }
+}
+}
 
-SDL_Delay(3000); 
+row = sizeof(option)/sizeof(option[0]);
+column = sizeof(option[0])/row;
+for (int i = 0; i < row; i++)
+{
+   for (int k = 0; k < column; k++) {
+    if(option[i][k] == 1){
+        SDL_SetRenderDrawColor(renderer,255,255,255,255);
+        SDL_Rect rect = {SCREEN_WIDTH/100*40+(k*10),SCREEN_HEIGHT/100*45+(i*10),10,10};
+        SDL_RenderDrawRect(renderer, &rect);
+        SDL_RenderFillRect(renderer, &rect);
+    }
+}
+}
+
+row = sizeof(leave)/sizeof(leave[0]);
+column = sizeof(leave[0])/row;
+for (int i = 0; i < row; i++)
+{
+   for (int k = 0; k < column; k++) {
+    if(leave[i][k] == 1){
+        SDL_SetRenderDrawColor(renderer,255,255,255,255);
+        SDL_Rect rect = {SCREEN_WIDTH/100*40+(k*10),SCREEN_HEIGHT/100*60+(i*10),10,10};
+        SDL_RenderDrawRect(renderer, &rect);
+        SDL_RenderFillRect(renderer, &rect);
+    }
+}
+}
+
+/*
+row = sizeof(arrow)/sizeof(arrow[0]);
+column = sizeof(arrow[0])/row;
+for (int i = 0; i < row; i++)
+{
+   for (int k = 0; k < column; k++) {
+    if(arrow[i][k] == 1){
+        SDL_SetRenderDrawColor(renderer,255,255,255,255);
+        SDL_Rect rect = {200+(k*10),400+(i*10),10,10};
+        SDL_RenderDrawRect(renderer, &rect);
+        SDL_RenderFillRect(renderer, &rect);
+    }
+}
+}
+*/
+
+/*
+row = sizeof(gameover)/sizeof(gameover[0]);
+column = sizeof(gameover[0])/row;
+for (int i = 0; i < row; i++)
+{
+   for (int k = 0; k < column; k++) {
+    if(gameover[i][k] == 1){
+        SDL_SetRenderDrawColor(renderer,130,0,0,0);
+        SDL_Rect rect = {100+(k*20),300+(i*20),20,20};
+        SDL_RenderDrawRect(renderer, &rect);
+        SDL_RenderFillRect(renderer, &rect);
+    }
+}
+}
+
+row = sizeof(numbers)/sizeof(numbers[0]);
+column = sizeof(numbers[0])/row;
+for (int i = 0; i < row; i++)
+{
+   for (int k = 0; k < column; k++) {
+    if(numbers[i][k] == 1){
+        SDL_SetRenderDrawColor(renderer,130,0,0,0);
+        SDL_Rect rect = {100+(k*20),300+(i*20),20,20};
+        SDL_RenderDrawRect(renderer, &rect);
+        SDL_RenderFillRect(renderer, &rect);
+    }
+}
+}
+*/
+
+SDL_Delay(1000); 
 SDL_RenderPresent(renderer);
-SDL_Delay(300); 
-SDL_DestroyRenderer(renderer);
+SDL_Delay(5000); 
+int esc = 0;
+	
+	const Uint8* keystates = SDL_GetKeyboardState( 0 );
 
-SDL_DestroyWindow(fenetre);
-SDL_Quit();
+if(esc = 0 && keystates [SDL_SCANCODE_ESCAPE]){
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(fenetre);
+    SDL_Quit();
+}
+
+
 }
